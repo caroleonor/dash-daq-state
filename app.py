96db93c37dd4ff07750860678845a2597bd6daa5
Austin Lasseter <austinlasseter@gmail.com>
2019-11-02 15:54:56 -0400

    updated requirements

--- app.py
+++ app.py
@@ -5,16 +5,13 @@ import plotly.graph_objs as go
 from dash.dependencies import Input, Output, State
 import plotly.figure_factory as ff
 import pandas as pd
+import dash_daq as daq
 
 ########### Define a few variables ######
 
-tabtitle = 'DC Properties'
-sourceurl = 'https://plot.ly/python/scattermapbox/'
-githublink = 'https://github.com/austinlasseter/dc-properties-map'
-mapbox_access_token = open("assets/mytoken.mapbox_token").read()
-df = pd.read_csv('resources/DC_Properties.csv', index_col='Unnamed: 0')
-df = df.sample(500)
-varlist=['BATHRM', 'HF_BATHRM', 'ROOMS', 'BEDRM', 'STORIES',  'PRICE']
+tabtitle = 'Dash DAQ'
+sourceurl = 'https://dash.plot.ly/dash-daq'
+githublink = 'https://github.com/austinlasseter/dash-daq-state'
 
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -22,59 +19,11 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 app.title=tabtitle
 
-########## Figure
-
-def make_my_cool_figure(some_value):
-    fig = go.Figure(go.Scattermapbox(
-            lat=df['LATITUDE'],
-            lon=df['LONGITUDE'],
-            mode='markers',
-            marker=go.scattermapbox.Marker(
-                size=9,
-                colorscale='Reds',
-                color=df[some_value]
-            ),
-            text=df['ASSESSMENT_SUBNBHD']
-
-        ))
-
-    fig.update_layout(
-        autosize=True,
-        hovermode='closest',
-        mapbox=go.layout.Mapbox(
-            accesstoken=mapbox_access_token,
-            bearing=0,
-            center=go.layout.mapbox.Center(
-                lat=38.92,
-                lon=-77.07
-            ),
-            pitch=0,
-            zoom=10
-        ),
-    )
-    return fig
-
-
 ########### Layout
 
 app.layout = html.Div(children=[
-    html.H1('DC Properties'),
-    # Dropdowns
-    html.Div(children=[
-        # left side
-        html.Div([
-                html.H6('Select a variable for colorscale:'),
-                dcc.Dropdown(
-                    id='stats-drop',
-                    options=[{'label': i, 'value': i} for i in varlist],
-                    value='PRICE'
-                ),
-        ], className='three columns'),
-        # right side
-        html.Div([
-            dcc.Graph(id='dc-map')
-        ], className='nine columns'),
-    ], className='twelve columns'),
+    html.H1('This is the header'),
+
 
     # Footer
     html.Br(),
@@ -83,13 +32,6 @@ app.layout = html.Div(children=[
     html.A("Data Source", href=sourceurl),
     ]
 )
-
-############ Callbacks
-
-@app.callback(Output('dc-map', 'figure'),
-                [Input('stats-drop', 'value')])
-def generate_map(dropdown_chosen_value):
-    return make_my_cool_figure(dropdown_chosen_value)
 
 
 ############ Deploy
